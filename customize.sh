f220c6360141273e4a0d23c32d088b39f5c31a5b
Jerbe <lutie@DESKTOP-DPOHM6C.localdomain>
2022-05-28 13:28:08 +0800

    修改

--- customize.sh
+++ customize.sh
@@ -2,3 +2,10 @@
 #===============================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' openwrt/package/base-files/files/bin/config_generate
+
+#2. 改默认WIFI SSID名
+sed -i 's/ssid=OpenWrt/ssid=D-WiFi/g' openwrt/package/kernel/mac80211/files/lib/wifi
+sed -i 's/SSID1=OpenWrt_5G/SSID1=D-WiFi_5G/g' openwrt/package/lean/mt/drivers/mt_wifi/files/mt7612.dat
+sed -i 's/SSID1=OpenWrt_5G/SSID1=D-WiFi_5G/g' openwrt/package/lean/mt/drivers/mt_wifi/files/mt7615.1.5G.dat
+sed -i 's/SSID1=OpenWrt_5G/SSID1=D-WiFi_5G/g' openwrt/package/lean/mt/drivers/mt_wifi/files/mt7615.5G.dat
+sed -i 's/SSID1=OpenWrt_5G/SSID1=D-WiFi_5G/g' openwrt/package/lean/mt/drivers/mt_wifi/files/mt7615.dat
