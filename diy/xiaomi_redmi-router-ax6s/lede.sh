120e0d70b48e48a99673fdf7a6341b28e8c7985f
Jerbe <lutie@DESKTOP-DPOHM6C.localdomain>
2022-05-31 16:12:29 +0800

    fix

--- diy/xiaomi_redmi-router-ax6s/lede.sh
+++ diy/xiaomi_redmi-router-ax6s/lede.sh
@@ -5,7 +5,7 @@ sed -i 's/192.168.1.1/192.168.31.1/g' openwrt/package/base-files/files/bin/confi
 
 #2. 改默认WIFI SSID名
 sed -i 's/ssid=OpenWrt/ssid=D-WiFi/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/SSID1=OpenWrt_5G/SSID1=D-WiFi_5G/g' openwrt/package/lean/mt/drivers/mt_wifi/files/mt7612.dat
+sed -i 's/SSID1=OpenWrt_5G/SSID1=D-WiFi_5G/g' openwrt/package/kernel/mt-drivers/mt_wifi/files/mt7612.dat
 sed -i 's/SSID1=OpenWrt_5G/SSID1=D-WiFi_5G/g' openwrt/package/lean/mt/drivers/mt_wifi/files/mt7615.1.5G.dat
 sed -i 's/SSID1=OpenWrt_5G/SSID1=D-WiFi_5G/g' openwrt/package/lean/mt/drivers/mt_wifi/files/mt7615.5G.dat
 sed -i 's/SSID1=OpenWrt_5G/SSID1=D-WiFi_5G/g' openwrt/package/lean/mt/drivers/mt_wifi/files/mt7615.dat
